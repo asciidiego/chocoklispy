2fc1f91a857f411b32c546b40f0ac6a7f7429644
diego <diego.vincent.rodriguez@gmail.com>
2022-07-24 12:36:36 +0200

    feat: add a struct!

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-  puts("Hello, world!");
+  struct point {
+    float x;
+    float y;
+  };
+  struct point p1;
+  p1.x = 1;
+  p1.y = 2;
+  printf("Point defined: [%f, %f]", p1.x, p1.y);
   return 0;
 }
