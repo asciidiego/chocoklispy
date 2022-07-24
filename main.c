055fc4974fb17ed10dd5c7e6d8804741a5d3998d
diego <diego.vincent.rodriguez@gmail.com>
2022-07-24 13:17:09 +0200

    feat: add editable input and history

--- main.c
+++ main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-// Declare a buffer for user input of size 2048
-static char input[2048];
+#include <editline/readline.h>
 
 
 int main(int argc, char** argv) {
   // Print version and exit info
-  puts("Chococlispy version 0.0.0.0.1");
+  puts("Chococlispy version 0.0.0.0.2");
   puts("Press Ctrl + C to exit.\n");
 
   while (1) {
-    // Output our prompt
-    fputs("chococlispy> ", stdout);
+    // output our prompt
+    char* input =  readline("chococlispy> ");
 
-    // Read a userline (max size of 2048)
-    fgets(input, 2048, stdin);
+    // add input to history
+    add_history(input);
 
-    // Echoes input back
-    printf("%s", input);
+    // echo input back
+    printf("%s\n", input);
+
+    // free memory
+    free(input);
   }
 
   return 0;
