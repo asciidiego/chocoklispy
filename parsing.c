c14e911e58b5386ae4c2a36dfbe9e744f0c3fd71
diego <diego.vincent.rodriguez@gmail.com>
2022-07-29 02:05:47 +0200

    feat: evaluate

--- parsing.c
+++ parsing.c
@@ -6,11 +6,53 @@ char *__PLATFORM__ = "Linux";
 char *__PLATFORM__ = "Windows";
 #endif
 
+#define DEBUG false
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <editline/readline.h>
 
 #include "mpc.h"
+
+double eval_op(char* op, double x, double y) {
+  if (strcmp(op, "+") == 0) {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0) {
+    return x - y;
+  }
+  if (strcmp(op, "*") == 0) {
+    return x * y;
+  }
+  if (strcmp(op, "/") == 0) {
+    return x / y;
+  }
+  return 0;
+}
+
+
+double eval(mpc_ast_t* ast) {
+  // if number, simply return it
+  if (strstr(ast->tag, "number")) {
+    return atof(ast->contents);
+  }
+
+  // the first child is "(" always; we can skip it.
+  char* operator = ast->children[1]->contents;
+
+  // like with reduce, we start with the first child...
+  double accum = eval(ast->children[2]);
+
+  // ... and iterate with the remaining children
+  int i = 3;
+  while (strstr(ast->children[i]->tag, "expr")) {
+    accum = eval_op(operator, accum, eval(ast->children[i]));
+    i++;
+  }
+
+  return accum;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -31,7 +73,8 @@ int main(int argc, char **argv)
             Number, Operator, Expr, Chocoklispy);
 
   // Print version and exit info
-  puts("Chocoklispy version 0.0.0.0.3");
+  char* VERSION = "0.0.0.0.4";
+  printf("Chocoklispy version %s\n", VERSION);
   puts("Press Ctrl + C to exit.");
   printf("Platform: %s\n\n", __PLATFORM__);
 
@@ -43,12 +86,17 @@ int main(int argc, char **argv)
     // add input to history
     add_history(input);
 
-    // echo input back
+    // parsing result
     mpc_result_t result;
 
     if (mpc_parse("<stdin>", input, Chocoklispy, &result))
     {
+      #if DEBUG
       mpc_ast_print(result.output);
+      #endif
+
+      double eval_result = eval(result.output);
+      printf("%f\n", eval_result);
       mpc_ast_delete(result.output);
     }
     else
